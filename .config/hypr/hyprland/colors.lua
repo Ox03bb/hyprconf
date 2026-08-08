9d71c1c6c715190cb1fb767002ed8bf8ee55f2e3
ox03bb <ali.de.bouakaz@gmail.com>
2026-08-08 16:12:47 +0100

    first commit

---
+++ .config/hypr/hyprland/colors.lua
@@ -0,0 +1,16 @@
+hl.config({
+    general = {
+        col = {
+            active_border   = "rgba(4a464477)",
+            inactive_border = "rgba(1e1b1933)",
+        },
+    },
+    misc = {
+        background_color = "rgba(151311FF)",
+    },
+})
+
+hl.window_rule({
+    match        = { pin = 1 },
+    border_color = "rgba(dcc2adAA) rgba(dcc2ad77)",
+})
